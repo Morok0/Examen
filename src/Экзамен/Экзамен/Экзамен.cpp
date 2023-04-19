0de9906ef3179172da64d891e39a94861fc2adb2
Morok0 <95218742+Morok0@users.noreply.github.com>
2023-04-19 10:22:15 +0300

    Add files via upload
    Создание структуры и класса

---
+++ src/Экзамен/Экзамен/Экзамен.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+struct Newspaper {
+	string title;
+	string owner;
+	int pages;
+};
+
+class PostBox {
+private:
+	Newspaper** papers;
+	int num_papers;
+};
+int main() {
+	
+	return 0;
+}
