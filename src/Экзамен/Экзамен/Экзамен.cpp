21b3cb23540d7581b4a0f9f8da6dbef500ef8138
Morok0 <95218742+Morok0@users.noreply.github.com>
2023-04-19 10:39:05 +0300

    Add files via upload
    Добавление новой газеты и вывод в файл

--- src/Экзамен/Экзамен/Экзамен.cpp
+++ src/Экзамен/Экзамен/Экзамен.cpp
@@ -30,7 +30,7 @@ public:
 
 	// деструктор
 	~PostBox() {
-		
+
 		for (int i = 0; i < num_papers; i++) {
 			if (papers[i] != NULL) {
 				delete papers[i];
@@ -40,7 +40,34 @@ public:
 		//освобождение памяти
 		delete[] papers;
 	}
-
+	// добавление газеты
+	void addNewspaper(string title, string owner, int pages) {
+	
+		for (int i = 0; i < num_papers; i++) {
+			if (papers[i] == NULL) {
+				//идёт сохранение указателей экземпляра газеты в массив 
+				//перед этим ишется первый пустой элемент массива
+				papers[i] = new Newspaper;
+			    papers[i]->title = title;
+				papers[i]->owner = owner;
+				papers[i]->pages = pages;
+				break;
+			}
+		}
+	}
+	//cj[hfytybt d afqlk
+	void saveToFile(string filename) {
+		ofstream fout(filename);
+		for (int i = 0; i < num_papers; i++) {
+			if (papers[i] != NULL) {
+				fout << papers[i]->title << endl;
+				fout << papers[i]->owner << endl;
+				fout << papers[i]->pages << endl;
+			}
+		}
+		// закрываем файл
+		fout.close();
+	}
 };
 int main() {
 	
